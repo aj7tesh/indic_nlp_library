ff5779afd25b82bdf6ac8280e1443fea9d7a42e9
aj7tesh <ajitesh.sharma@tarento.com>
2019-05-29 12:53:16 +0530

    setup file update

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(name='indicnlp_anuvada-py',
-      description='indic nlp tokenization',
-      version='0.0.2',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-      packages=['src','src.indicnlp'])
+setuptools.setup(
+    name="indic-nlp-anuvada",
+    version="0.0.1",
+    # author="Example Author",
+    # author_email="author@example.com",
+    description="A indicnlp package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aj7tesh/indic_nlp_library",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+name = "indicNlp_anuvada"
